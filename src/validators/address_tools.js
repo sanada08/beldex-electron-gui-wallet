52176432cdb4798675ae7309519439f18b6674a9
athenscoder <athenscoder@gmail.com>
2021-06-30 10:46:34 +0530

    Flash Update

--- src/validators/address_tools.js
+++ src/validators/address_tools.js
@@ -2688,7 +2688,7 @@ var mn_words = {
             "cell", "check", "cost", "darling", "during", "footstep", "fragile",
             "hallway", "hardly", "horizon", "invisible", "journey", "midnight", "mud",
             "nod", "pause", "relax", "shiver", "sudden", "value", "youth", "abuse",
-            "admire", "blink", "breast", "bruise", "constantly", "couple", "creep",
+            "admire", "flash", "breast", "bruise", "constantly", "couple", "creep",
             "curve", "difference", "dumb", "emptiness", "gotta", "honor", "plain",
             "planet", "recall", "rub", "ship", "slam", "soar", "somebody", "tightly",
             "weather", "adore", "approach", "bond", "bread", "burst", "candle",
