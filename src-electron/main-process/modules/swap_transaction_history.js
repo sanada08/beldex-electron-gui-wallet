d0d4819b7f5db311c90fac7648de3679d02756f5
alexsanchez@beldex.io <alexsanchez@beldex.io>
2023-08-11 17:59:22 +0530

    Order history updated.

---
+++ src-electron/main-process/modules/swap_transaction_history.js
@@ -0,0 +1,83 @@
+// const sqlite3 = require('sqlite3').verbose();
+// const { open } = require('sqlite');
+const fs = require("fs");
+const _ = require("lodash");
+
+export class SwapTxnHistory {
+  constructor(backend) {
+    this.backend = backend;
+  }
+
+  // let address = 'addr';
+  async getOrderHistory(address) {
+    try {
+      const data = fs.readFileSync("swap_transaction_history.json");
+      let jsonData = JSON.parse(data);
+      let transaction_id = jsonData[`${address}`];
+      return transaction_id;
+    } catch (error) {
+      console.error("error:,", error.message);
+    }
+  }
+
+  // let txn_id = 'mohammed_khan_rahman';
+  // let address = 'addr';
+  async updateTransactionDetails(txn_id, address) {
+    try {
+      const data = fs.readFileSync("swap_transaction_history.json");
+      let jsonData = JSON.parse(data);
+      let tx_id = [`${txn_id}`];
+      if (_.isEmpty(jsonData)) {
+        jsonData = { [`${address}`]: tx_id };
+        const jsonString = JSON.stringify(jsonData);
+        fs.writeFileSync(
+          "swap_transaction_history.json",
+          jsonString,
+          "utf-8",
+          err => {
+            if (err) throw err;
+            console.log("Data added to file");
+          }
+        );
+        return;
+      } else {
+        if (_.has(jsonData, address)) {
+          let data = jsonData[`${address}`];
+          let checkAlreadyExits = data.find(function(element) {
+            return element == tx_id[0];
+          });
+          if (!checkAlreadyExits) {
+            data.push(`${tx_id}`);
+            const jsonString = JSON.stringify(jsonData);
+            fs.writeFileSync(
+              "swap_transaction_history.json",
+              jsonString,
+              "utf-8",
+              err => {
+                if (err) throw err;
+                console.log("Data added to file");
+              }
+            );
+          }
+          return;
+        } else {
+          jsonData[`${address}`] = tx_id;
+          const jsonString = JSON.stringify(jsonData);
+          fs.writeFileSync(
+            "swap_transaction_history.json",
+            jsonString,
+            "utf-8",
+            err => {
+              if (err) throw err;
+              console.log("Data added to file");
+            }
+          );
+          return;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
